33234d765f4a04d744d5e415a2dd73ae697d0bf7
demouse021 <107237756+demouse021@users.noreply.github.com>
2022-06-15 18:09:20 +0530

    Update PHPMailer.php

--- src/PHPMailer.php
+++ src/PHPMailer.php
@@ -3237,31 +3237,6 @@ namespace PHPMailer\PHPMailer;
      *
      * @return string
      */
-    protected function attachAll($disposition_type, $boundary)
-    {
-        //Return text of body
-        $mime = [];
-        $cidUniq = [];
-        $incl = [];
-
-        //Add all attachments
-        foreach ($this->attachment as $attachment) {
-            //Check if it is a valid disposition_filter
-            if ($attachment[6] === $disposition_type) {
-                //Check for string attachment
-                $string = '';
-                $path = '';
-                $bString = $attachment[5];
-                if ($bString) {
-                    $string = $attachment[0];
-                } else {
-                    $path = $attachment[0];
-                }
-
-                $inclhash = hash('sha256', serialize($attachment));
-                if (in_array($inclhash, $incl, true)) {
-                    continue;
-                }
                 $incl[] = $inclhash;
                 $name = $attachment[2];
                 $encoding = $attachment[3];
